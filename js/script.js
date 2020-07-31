9023ee0ad871d02149c34073879107824e0a7f2e
leandromac <contato@leandromacedo.com>
2020-07-31 15:15:11 -0300

    upgrade

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-(function($){
+(function() {
   'use strict';
 
-})(window.DOM);
+  var $form = document.querySelector('[data-js="form-generator"]');
+  var $result = document.querySelector('[data-js="result-url"]');
+  var $phone = document.querySelector('[data-js="phone"]').value;
+  var $message = document.querySelector('[data-js="message"]').value;
+  var $generate = 'https://api.whatsapp.com/send?phone=+55' + $phone + '&text=' + $message;
+
+  $form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    $result.innerHTML = $generate;
+  }, false);
+
+  // var $formGenerator = $('[data-js="form-generator"]');
+  // var $phone = $('[data-js="phone"]').value.replace(/\D/g, '');
+  // var $message = $('[data-js="message"]').value.replace(/\s/g, '%20');
+  // var $url = 'https://api.whatsapp.com/send?phone=+55seunumerodetelefone&text=suamensagem'.replace(/seunumerodetelefone/g, $phone);
+
+})();
