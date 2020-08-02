96c64276bab828e0fa427177b1988a84f9831d25
leandromac <contato@leandromacedo.com>
2020-08-02 10:39:05 -0300

    Custom elements css

--- js/script.js
+++ js/script.js
@@ -1,40 +1,31 @@
 function buttonCopy() {
-  var copyText = document.querySelector('[data-js="result-url"]');
-  copyText.select();
-  copyText.setSelectionRange(0, 99999);
+  var copyUrl = document.querySelector('[data-js="result-url"]');
+  copyUrl.select();
+  copyUrl.setSelectionRange(0, 99999);
   document.execCommand("copy");
-  alert("URL Copiada com sucesso!" /* + copyText.value */);
+  alert("URL Copiada com sucesso!");
 }
 
 (function() {
   'use strict';
 
+  var $form = $('[data-js="form-generator"]');
+  var $result = $('[data-js="result-url"]');
+  var $phone = $('[data-js="phone"]');
+  var $message = $('[data-js="message"]');
+  var $url = 'https://api.whatsapp.com/send?phone=+55';
+
+  $form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    $result.value = getUrl();
+  }, false);
+
   function $(a) {
     return document.querySelector(a);
   }
 
-  var $form = $('[data-js="form-generator"]');
-  var $result = $('[data-js="result-url"]');
-  var $phone = $('[data-js="phone"]');
-  var $url = 'https://api.whatsapp.com/send?phone=+55';
-
-  function replacePhone() {
-    return $url+$phone.value.replace(/\D/g, '');
+  function getUrl() {
+    return 'https://api.whatsapp.com/send?phone=+55'+$phone.value.replace(/\D/g, '')+'&text='+$message.value.replace(/\s/g, '%20');
   }
 
-  $form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    $result.value = replacePhone();
-  }, false);
-
-  // var $a = document.createElement('a');
-  // var $linkText = document.createTextNode('Clique no botão para copiar o seu link!');
-  // $a.appendChild($linkText);
-  // $a.title = $url;
-  // $a.href = $url;
-  // $a.target = "_blank";
-  // $a.style = "color:#FFF";
-  // $a.className = "btn";
-  // $result.appendChild($a);
-
-})();
+})(window.DOM);
